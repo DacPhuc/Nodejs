d83d2ef4a13e2659d7252ea5382c807582340036
DacPhuc <phuc.nguyendac@hcmut.edu.vn>
2020-07-21 00:25:40 +0700

    "Add log node js"

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.get("/node/hello", (req, res) =>
-  res.status(200).send("Hello World Dac Phuc !")
-);
+app.get("/node/hello", (req, res) => {
+  console.log("Node js here running");
+  res.status(200).send("Hello World Dac Phuc !");
+});
 app.listen(3001, () => console.log("Server ready"));

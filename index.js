eba4a030d2999f2c37b32224ca23f5733495756a
DacPhuc <phuc.nguyendac@hcmut.edu.vn>
2020-07-21 00:20:37 +0700

    "Change URL"

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.status(200).send("Hello World Dac Phuc !"));
+app.get("/node/hello", (req, res) =>
+  res.status(200).send("Hello World Dac Phuc !")
+);
 app.listen(3001, () => console.log("Server ready"));
